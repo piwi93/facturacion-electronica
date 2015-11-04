c9eec504cfba74b7718ea321c6a52193ecfbcf38
Emanuel Laporta <emanuel.laporta@outlook.com>
2015-11-03 23:46:31 -0300

    Programa base

---
+++ src/main/java/org/fing/tecnoinf/app/Main.java
@@ -0,0 +1,25 @@
+//JDOM2 http://www.studytrails.com/java/xml/jdom2/java-xml-jdom2-introduction.jsp
+//Validacion de XSD online http://www.freeformatter.com/xml-validator-xsd.html
+//SAXPArser https://docs.oracle.com/javase/tutorial/jaxp/sax/parsing.html
+
+//Xml parsing data DOM & SAX: http://www.java-samples.com/showtutorial.php?tutorialid=152
+//Validar documentos XML usando XSD: http://www.bitriding.com/articulos/validacion-con-xsd.html
+//Validación de ficheros XML usando un esquema XSD (JAVA): http://jesus.hernandez.abeleira.com/2014/03/validacion-de-ficheros-xml-contra-un.html
+
+
+package org.fing.tecnoinf.app;
+
+public class Main {
+
+   public void main(String[] args) {
+
+   }
+
+   String validate(String xml, String xsd){
+	   return "";
+   }
+   
+   String parseData(String xml){
+	   return "";
+   }
+}
